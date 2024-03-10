025ba3d70a45e4d8e82e17aaa4210fe7a82af497
Vitalina Shapovalova <vladdislavovna@gmail.com>
2024-03-10 08:10:23 +0200

    Merge step4. Add Tests to LibraryManagerTests and LibraryStatisticsAnalyzerTests.

--- nunit-csharp-samples/Tests/LibraryManagerTests.cs
+++ nunit-csharp-samples/Tests/LibraryManagerTests.cs
@@ -1,6 +1,46 @@
-namespace nunit_csharp_samples.Tests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+[TestClass]
 public class LibraryManagerTests
 {
-    // TODO step 4.
+    [TestMethod]
+    public void AddBook_ValidBook_Success()
+    {
+        // Arrange
+        var manager = new LibraryManager();
+        var book = new Book { Title = "Book 1", Author = "Author 1" };
+
+        // Act
+        manager.AddBook(book);
+
+        // Assert
+        var books = manager.Books;
+        CollectionAssert.Contains(books, book);
+    }
+
+    [TestMethod]
+    public void AddBook_NullBook_ThrowsArgumentNullException()
+    {
+        // Arrange
+        var manager = new LibraryManager();
+
+        // Act & Assert
+        Assert.ThrowsException<ArgumentNullException>(() => manager.AddBook(null));
+    }
+
+    [TestMethod]
+    public void RemoveBook_ExistingBook_Success()
+    {
+        // Arrange
+        var manager = new LibraryManager();
+        var book = new Book { Title = "Book 1", Author = "Author 1" };
+        manager.AddBook(book);
+
+        // Act
+        manager.RemoveBook(book);
+
+        // Assert
+        var books = manager.Books;
+        Assert.AreEqual(0, books.Count);
+    }
 }
--- nunit-csharp-samples/Tests/LibraryStatisticsAnalyzerTests.cs
+++ nunit-csharp-samples/Tests/LibraryStatisticsAnalyzerTests.cs
@@ -1,6 +1,56 @@
-namespace nunit_csharp_samples.Tests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+[TestClass]
 public class LibraryStatisticsAnalyzerTests
 {
-    // TODO step 4.
+    [TestMethod]
+    public void GetBooksByAuthor_ValidAuthor_ReturnsBookList()
+    {
+        // Arrange
+        var manager = new LibraryManager();
+        var analyzer = new LibraryStatisticsAnalyzer(manager);
+        var author = "Author 1";
+        var expectedBook = new Book { Title = "Book 1", Author = author };
+        manager.AddBook(expectedBook);
+
+        // Act
+        var result = analyzer.GetBooksByAuthor(author);
+
+        // Assert
+        CollectionAssert.Contains(result, expectedBook);
+    }
+
+    [TestMethod]
+    public void GetBookByTitle_ValidTitle_ReturnsBook()
+    {
+        // Arrange
+        var manager = new LibraryManager();
+        var analyzer = new LibraryStatisticsAnalyzer(manager);
+        var title = "Book 1";
+        var expectedBook = new Book { Title = title, Author = "Author 1" };
+        manager.AddBook(expectedBook);
+
+        // Act
+        var result = analyzer.GetBookByTitle(title);
+
+        // Assert
+        Assert.AreEqual(expectedBook, result);
+    }
+
+    [TestMethod]
+    public void GetBookByArticul_ValidArticul_ReturnsBook()
+    {
+        // Arrange
+        var manager = new LibraryManager();
+        var analyzer = new LibraryStatisticsAnalyzer(manager);
+        var articul = "123456";
+        var expectedBook = new Book { Title = "Book 1", Author = "Author 1", Articul = articul };
+        manager.AddBook(expectedBook);
+
+        // Act
+        var result = analyzer.GetBookByArticul(articul);
+
+        // Assert
+        Assert.AreEqual(expectedBook, result);
+    }
 }
